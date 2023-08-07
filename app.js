65a97348b86ed5d4c8d0b8a4bc8e05b4c6f26a25
Lee Jeong Ho <73730434+ghdi1ghdi@users.noreply.github.com>
2023-08-07 18:28:40 +0900

    Feat: first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const a = 5;
+const b = 2;
+let myName = "jackun";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("Hello " + myName);
+
+myName = "phil jackun";
+console.log("your new name is " + myName);
